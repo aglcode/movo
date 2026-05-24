497aacf45032643ad6cddff9fbc7c1f55af2341d
aglcode <webc26696@gmail.com>
2026-05-24 20:44:03 +0800

    notice

--- src/components/FloatingDevelopmentAlert.jsx
+++ src/components/FloatingDevelopmentAlert.jsx
@@ -11,8 +11,8 @@ const FloatingDevelopmentAlert = () => {
   return (
     <div className="fixed bottom-5 left-5 z-[100] w-[calc(100%-2.5rem)] max-w-sm sm:bottom-6 sm:left-6">
       <Alert className="border-white/10 bg-black/80 text-white shadow-2xl shadow-black/40 backdrop-blur-md">
-        <AlertCircleIcon className="text-[#E50914]" />
-        <AlertTitle>Website currently under development</AlertTitle>
+        <AlertCircleIcon className="text-yello-500" />
+        <AlertTitle><span className='text-yellow-500'>Notice:</span> Website currently under development</AlertTitle>
         <AlertDescription className="text-white/70">
           From Dev: gelo
         </AlertDescription>
